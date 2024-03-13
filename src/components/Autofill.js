de7e9f0380a0d7338fb2995085114918e0f6861e
Lauri Talvitie <lauri.talvitie@hotmail.com>
2024-03-13 17:54:53 +0200

    styling added

--- src/components/Autofill.js
+++ src/components/Autofill.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import Autocomplete from "react-google-autocomplete";
+import { styled } from '@mui/material/styles';
+
+const StyledAutocomplete = styled(Autocomplete)({
+  width: '85%',
+  padding: '10px', 
+  fontSize: '16px',
+  borderRadius: '4px', 
+  border: '2px solid #ccc',
+  textAlign: 'center',
+  display: 'flex', // Use flexbox
+  justifyContent: 'center', // Center horizontally
+  alignItems: 'center', // Center vertically
+});
 
 const Autofill = () => {
-
   return (
-    <div>
-        <Autocomplete apiKey={process.env.REACT_APP_GOOGLE_API_KEY}/>;
+    <div style={{display: 'flex', justifyContent: 'center'}}>
+      <StyledAutocomplete apiKey={process.env.REACT_APP_GOOGLE_API_KEY} />
     </div>
   );
 };
