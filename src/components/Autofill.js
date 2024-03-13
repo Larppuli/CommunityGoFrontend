ae48a9280d9386e5bae6624bc1d2ae3a7d07b519
Lauri Talvitie <lauri.talvitie@hotmail.com>
2024-03-13 19:07:58 +0200

    implementation of a textfield with google maps autofill

--- src/components/Autofill.js
+++ src/components/Autofill.js
@@ -1,24 +1,34 @@
 import React from 'react';
-import Autocomplete from "react-google-autocomplete";
-import { styled } from '@mui/material/styles';
+import { LoadScript, Autocomplete } from '@react-google-maps/api';
+import { styled } from '@mui/system';
 
-const StyledAutocomplete = styled(Autocomplete)({
-  width: '85%',
-  padding: '10px', 
-  fontSize: '16px',
-  borderRadius: '4px', 
-  border: '2px solid #ccc',
-  textAlign: 'center',
-  display: 'flex', // Use flexbox
-  justifyContent: 'center', // Center horizontally
-  alignItems: 'center', // Center vertically
-});
+const Autofill = ({defaultText}) => {
 
-const Autofill = () => {
+    const StyledAutocomplete = styled(Autocomplete)({
+        width: '90%',
+        margin: 'auto',
+        '& input': {
+          width: '90%',
+          padding: '10px',
+          fontSize: '21px',
+          borderRadius: '4px',
+          border: '2px solid #ccc',
+          textAlign: 'center',
+          '::placeholder': { 
+            textAlign: 'center',
+          },
+        },
+      });
+
   return (
-    <div style={{display: 'flex', justifyContent: 'center'}}>
-      <StyledAutocomplete apiKey={process.env.REACT_APP_GOOGLE_API_KEY} />
-    </div>
+    <LoadScript
+      googleMapsApiKey={process.env.REACT_APP_GOOGLE_API_KEY}
+      libraries={['places']}
+    >
+      <StyledAutocomplete>
+        <input type="text" placeholder={text}/>
+      </StyledAutocomplete>
+    </LoadScript>
   );
 };
 
