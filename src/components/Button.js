2b6a1e975ccffe927039fb2e58bf4120c503900b
Lauri Talvitie <lauri.talvitie@hotmail.com>
2024-03-16 01:15:19 +0200

    Implementation of a button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,12 +1,32 @@
 import React from 'react';
 import Button from '@mui/material/Button';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
-const Button = ({buttonText}) => {
+// Create a custom theme for the button
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#1a237e',
+    },
+  },
+});
+
+const styles = {
+  root: {
+    width: '100%',
+    height: "50px",
+    margin: 'auto',
+  },
+};
+
+const MyButton = ({ buttonText, handleClick }) => {
   return (
-    <Button variant="contained" color="primary">
-      {buttonText}
-    </Button>
+    <ThemeProvider theme={theme}>
+      <Button variant="contained" color="primary" style={styles.root} onClick={handleClick}>
+        {buttonText}
+      </Button>
+    </ThemeProvider>
   );
 };
 
-export default Button;
+export default MyButton;
