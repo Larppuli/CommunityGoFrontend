f89eb262abe1109ebfbd517e8c30a51df8eed302
Lauri Talvitie <lauri.talvitie@hotmail.com>
2024-03-15 13:05:13 +0200

    edited the visuality of the componen and added comments

--- src/components/Autofill.js
+++ src/components/Autofill.js
@@ -1,10 +1,12 @@
 import React, { useRef, useEffect, useState } from 'react';
 import { Loader } from '@googlemaps/js-api-loader';
+import TextField from '@mui/material/TextField';
 
-const Autofill = ({ onPlaceSelected }) => {
+const Autofill = ({ onPlaceSelected, defaultText }) => {
   const inputRef = useRef(null);
   const [google, setGoogle] = useState(null);
 
+  // Initialize Google Maps Loader with the provided API key and load the 'places' library.
   useEffect(() => {
     const loader = new Loader({
       apiKey: process.env.REACT_APP_GOOGLE_API_KEY,
@@ -12,28 +14,54 @@ const Autofill = ({ onPlaceSelected }) => {
       libraries: ['places']
     });
 
+    // Set the 'google' state once the loader is successfully loaded.
     loader.load().then((google) => {
       setGoogle(google);
     });
   }, []);
 
   useEffect(() => {
+    // Set up Google Maps autocomplete functionality when the 'google' object is ready.
     if (!google) return;
 
     const autocomplete = new google.maps.places.Autocomplete(inputRef.current);
     
+    // Listen for changes in the selected place and call 'onPlaceSelected' with the updated place.
     autocomplete.addListener('place_changed', () => {
       const place = autocomplete.getPlace();
       onPlaceSelected(place);
     });
 
+    // Clean up event listeners on unmount.
     return () => {
       google.maps.event.clearInstanceListeners(autocomplete);
     };
   }, [google, onPlaceSelected]);
 
+  // TextField component with Google Maps Autocomplete is returned
   return (
-    <input ref={inputRef} type="text" placeholder="Enter a location" />
+    <TextField
+      inputRef={inputRef}
+      label={
+        <span style={{ color: '#d4d4dc' }}>{defaultText}</span>}
+      variant="filled"
+      fullWidth
+      sx={{
+        width: '96%',
+        '& .MuiFilledInput-root': {
+          borderColor: '#ffffff',
+        },
+        '& .MuiFilledInput-root:hover': {
+          borderColor: '#d4d4dc', 
+        },
+        '& .MuiInputLabel-outlined': {
+          color: '#d4d4dc',
+        },
+        '& .MuiFilledInput-input': {
+          color: '#ffffff',
+        },
+      }}
+    />
   );
 };
 
