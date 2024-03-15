8a4f039cf928ccfebf3dd4aba38a5fe80d4b4e1a
Lauri Talvitie <lauri.talvitie@hotmail.com>
2024-03-16 01:18:13 +0200

    implementation of saving a ride to database

--- src/App.js
+++ src/App.js
@@ -1,16 +1,77 @@
-import React from 'react';
-import { Typography } from '@mui/material';
+import React, { useState } from 'react';
+import { Typography, Alert, Grow } from '@mui/material';
 import Locations from './components/Locations';
 import MapContainer from './components/MapContainer';
+import MyButton from './components/Button';
+import DateTimePick from './components/DateTimePick';
 
 function App() {
+  const [destination, setDestination] = useState('');
+  const [pickup, setPickup] = useState('');
+  const [time, setTime] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleDestinationSelect = (destinationPlace) => {
+    setDestination(destinationPlace);
+  };
+
+  const handlePickupSelect = (pickupPlace) => {
+    setPickup(pickupPlace);
+  };
+
+  const handleTimeSelect = (time) => {
+    console.log(time)
+    setTime(time);
+  };
+
+  const handleSave = async () => {
+    if (!pickup || !destination || !time) {
+      setErrorMessage('Time, pickup or destination cannot be empty.');
+
+      setTimeout(() => {
+        setErrorMessage('');
+      }, 3000);
+
+      return;
+    }
+
+    const newRide = {
+      destination: destination.geometry,
+      pickup: pickup.geometry,
+      time: time
+    }
+
+    try {
+      await fetch(`${process.env.REACT_APP_SERVER_URI}rides`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(newRide),
+      });
+      
+      setErrorMessage('');
+    } catch (error) {
+      console.error('Error saving ride:', error);
+    };
+
+    setErrorMessage('');
+  };
+
   return (
     <div>
       <Typography variant="h4" align="center" gutterBottom style={{ color: '#d4d4dc' }}>
         <b>CommunityGo</b>
       </Typography>
       <MapContainer />
-      <Locations />
+      <Locations onPickupSelect={handlePickupSelect} onDestinationSelect={handleDestinationSelect}/>
+      <DateTimePick onTimeSelect={handleTimeSelect}/>
+      <MyButton buttonText="Save" handleClick={handleSave} />
+      <Grow in={!!errorMessage} timeout={300}>
+        <Alert severity="warning" variant="filled" style={{ marginTop: '10px' }}>
+          {errorMessage}
+        </Alert>
+      </Grow>
     </div>
   );
 }
