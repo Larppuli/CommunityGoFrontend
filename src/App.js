bef5d8d8ba95fe01e8d197875ad10e997d90059e
Lauri Talvitie <lauri.talvitie@hotmail.com>
2024-03-15 12:50:15 +0200

    made simple structure for the app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
+import React from 'react';
+import { Typography } from '@mui/material';
+import Locations from './components/Locations';
+import MapContainer from './components/MapContainer';
+
 function App() {
   return (
     <div>
+      <Typography variant="h4" align="center" gutterBottom style={{ color: '#d4d4dc' }}>
+        <b>CommunityGo</b>
+      </Typography>
+      <MapContainer />
+      <Locations />
     </div>
   );
 }
