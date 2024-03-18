f21410bd0e8145c48d63e99575e99559b3f7884a
Lauri Talvitie <lauri.talvitie@hotmail.com>
2024-03-18 15:48:31 +0200

    edited to take more props

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,28 +2,26 @@ import React from 'react';
 import Button from '@mui/material/Button';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 
-// Create a custom theme for the button
+const MyButton = ({ buttonText, handleClick, backgroundColor, margin, width, height }) => {
+
+  // Create a custom theme for the button
 const theme = createTheme({
   palette: {
     primary: {
-      main: '#381494',
+      main: backgroundColor,
     },
+  },
+  root: {
+    width: width,
+    height: height,
+    margin: margin,
+    color: 'white'
   },
 });
 
-const styles = {
-  root: {
-    width: '100%',
-    height: "50px",
-    margin: 'auto',
-    color: '#d4d4dc'
-  },
-};
-
-const MyButton = ({ buttonText, handleClick }) => {
   return (
     <ThemeProvider theme={theme}>
-      <Button variant="contained" color="primary" style={styles.root} onClick={handleClick}>
+      <Button variant="contained" color="primary" style={theme.root} onClick={handleClick}>
         {buttonText}
       </Button>
     </ThemeProvider>
