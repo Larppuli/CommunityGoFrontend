128153c629c215ae69087482227613d111d55bf6
Lauri Talvitie <lauri.talvitie@hotmail.com>
2024-03-24 22:00:35 +0200

    passed loader as a prop to NewRideView and bundles

--- src/App.js
+++ src/App.js
@@ -3,13 +3,21 @@ import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Navbar from './components/Navbar';
 import NewRideView from './components/NewRideView';
 import Bundles from './components/Bundles';
+import { Loader } from '@googlemaps/js-api-loader';
 
 function App() {
+
+  const loader = new Loader({
+    apiKey: process.env.REACT_APP_GOOGLE_API_KEY,
+    version: "weekly",
+    libraries: ["places", "geometry"],
+  });
+
   return (
     <Router>
         <Routes>
-          <Route path="/new-ride" element={<NewRideView/>} />
-          <Route path="/bundles" element={<Bundles/>} />
+          <Route path="/new-ride" element={<NewRideView loader={loader} />} />
+          <Route path="/bundles" element={<Bundles loader={loader} />} />
         </Routes>
         <Navbar />
     </Router>
