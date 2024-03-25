5554cae36fc7ddbcce0e64f90c428980bb4e40c0
Lauri Talvitie <lauri.talvitie@hotmail.com>
2024-03-25 19:02:45 +0200

    changed component name

---
+++ src/components/PassengerBundle.js
@@ -0,0 +1,150 @@
+import React, { useState, useEffect } from 'react';
+import Card from '@mui/material/Card';
+import { Typography, Grow, Alert } from '@mui/material'; // Import Grow and Alert components
+import IconButton from '@mui/material/IconButton';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import { styled } from '@mui/material/styles';
+import Collapse from '@mui/material/Collapse';
+import Autofill from './Autofill';
+import MyButton from './Button';
+import axios from 'axios';
+
+const ExpandMore = styled((props) => {
+  const { expand, ...other } = props;
+  return <IconButton {...other} />;
+})(({ theme, expand }) => ({
+  transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
+  marginLeft: 'auto',
+  transition: theme.transitions.create('transform', {
+    duration: theme.transitions.duration.shortest,
+  }),
+}));
+
+function PassengerBundle( {ride, loader} ) {
+  const [expanded, setExpanded] = React.useState(false);
+  const arrivalTime = new Date(ride.arrivalTime);
+  const hours = arrivalTime.getHours().toString().padStart(2, '0');
+  const minutes = arrivalTime.getMinutes().toString().padStart(2, '0');
+  const [rideTime, setRideTime] = useState(null);
+  const [showAlert, setShowAlert] = useState(false);
+  const [message, setMessage] = useState('');
+  const [severity, setSeverity] = useState('');
+  const [id, setId] = useState('');
+  const [waypoints, setWaypoints] = useState([]);
+  const [disabled, setDisabled] = useState(true);
+  const [routes, setRoutes] = useState('');
+
+  useEffect(() => {
+    setRideTime(ride.rideTime)
+    setId(ride._id)
+    setWaypoints(ride.waypoints)
+  }, [ride.rideTime, ride._id, ride.waypoint, ride.waypoints, loader]);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
+  const handlePickupSave = async () => {
+
+    const newRideData = {
+        waypoints: waypoints,
+        rideTime: rideTime,
+        routes: routes
+    }
+
+    try {
+        await fetch(`${process.env.REACT_APP_SERVER_URI}rides/${id}`, {
+            method: 'PUT',
+            headers: {
+            'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(newRideData),
+        });
+        
+        } catch (error) {
+        console.error('Error saving ride:', error);
+        };
+  };
+
+  const calculateDuration = async (waypoint) => {
+
+    // Add every waypoint to an array
+    const waypointCleaned = {
+        geometry: waypoint.geometry,
+        address_components: waypoint.address_components,
+        name: waypoint.name
+    }
+    console.log("waypoints: ", ride.waypoints)
+    const stops = Array.isArray(ride.waypoints) ? [...ride.waypoints, waypointCleaned] : [waypointCleaned];
+
+    try {
+        const rideData = {
+            pickup: ride.pickup.geometry.location,
+            destination: ride.destination.geometry.location,
+            waypoints: stops
+          };
+
+        const response = await axios.post('http://localhost:5000/calculate-ride-time', rideData);
+        const { ride_time, routes } = response.data;
+        
+        const timeDifference = ride_time - rideTime;
+        console.log(timeDifference)
+
+        // If timeDifference is greater than 5, set showAlert to true
+        if (timeDifference > 5) {
+          setShowAlert(true);
+          setMessage("You are too far from the route!");
+          setSeverity("error");
+          // Set a timeout to hide the alert after 5 seconds
+          setTimeout(() => {
+            setShowAlert(false);
+          }, 5000);
+        } else {
+          setShowAlert(true);
+          setMessage("You are able to join the ride!");
+          setSeverity("success");
+          setDisabled(false)
+          setRideTime(ride_time)
+          setWaypoints(stops);
+          setRoutes(routes)
+          // Set a timeout to hide the alert after 5 seconds
+          setTimeout(() => {
+            setShowAlert(false);
+          }, 5000);
+      }
+    } catch (error) {
+      console.error('Error calculating ride duration:', error);
+    }
+  };
+
+  return (
+    <Card variant="outlined" style={{ color: 'white', background: 'linear-gradient(to right, #381494, #592ec7)', marginBottom: '18px', paddingBottom: "13px", marginInline: '10px', paddingInline: '20px' }}>
+      <Typography variant="h6" align="center">
+        <b>{ride.destination.name}</b>
+      </Typography>
+      <Typography align="center">
+        Taxi arrives to the destination at <b>{hours}:{minutes}</b>
+      </Typography>
+      <ExpandMore
+        expand={expanded}
+        onClick={handleExpandClick}
+        aria-expanded={expanded}
+        aria-label="show more"
+        style={{ color: "white", marginInline: "45%" }}
+      >
+      <ExpandMoreIcon color="white" />
+      </ExpandMore>
+      <Collapse in={expanded} timeout="auto" unmountOnExit >
+        <Autofill onPlaceSelected={calculateDuration} loader={loader.loader} defaultText="Your pickup location" margin="10px"/>
+        <MyButton handleClick={handlePickupSave} disabled={disabled} buttonText="Join ride" backgroundColor="#4B4B4B" width="100%" height="50px" />
+        <Grow in={showAlert} timeout={300} style={{ height: '40px' }} >
+          <Alert severity={severity} variant="filled" style={{ marginTop: '10px' }}>
+            {message}
+          </Alert>
+        </Grow>
+      </Collapse>
+    </Card>
+  );
+}
+
+export default PassengerBundle;
---
+++ src/components/PassengerBundles.js
@@ -0,0 +1,59 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import PassengerBundle from './PassengerBundle';
+import { Typography, List } from '@mui/material';
+
+function PassengerBundles( loader ) {
+  const [rides, setRides] = useState([]);
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(true); // Add loading state
+  
+  useEffect(() => {
+    const fetchRides = async () => {
+      try {
+        const response = await axios.get(`${process.env.REACT_APP_SERVER_URI}rides`);
+        if (response.data.length > 0) {
+          setRides(response.data);
+        } else {
+          setError("No rides available");
+        }
+      } catch (error) {
+        console.error('Error fetching rides:', error);
+        setError("Error fetching rides");
+      } finally {
+        setIsLoading(false); // Set loading state to false when fetching completes
+      }
+    };
+
+    fetchRides();
+  }, [loader]);
+
+  return (
+    <div style={{ height: "800px", overflow: "hidden" }}>
+      <div style={{ position: "sticky", backgroundColor: "#333", zIndex: 1 }}>
+        <Typography variant="h4" align="center" gutterBottom style={{ color: 'white' }}>
+          <b>CommunityGo</b>
+        </Typography>
+      </div>
+      <div style={{ marginTop: "15%", position: "sticky", backgroundColor: "#333", zIndex: 1 }}>
+        <Typography variant="h5" align="center" gutterBottom style={{ color: 'white' }}>
+          <b>Rides in your local area</b>
+        </Typography>
+      </div>
+      <div style={{ maxHeight: "70%", overflow: 'auto', background: "#333333" }}>
+        <List>
+          {isLoading ? ( // Render loading message if still loading
+            <Typography  variant="h6" align="center" style={{ color: 'white' }}>Loading...</Typography>
+          ) : (
+            rides.map((ride, index) => (
+              <PassengerBundle key={index} ride={ride} loader={loader} />
+            ))
+          )}
+          {error && <Typography  variant="h6" align="center" style={{ color: 'white' }}>{error}</Typography>}
+        </List>
+      </div>
+    </div>
+  );
+}
+
+export default PassengerBundles;
