08b60828d2838a2f093ebafc49e9f79ed3806f49
Lauri Talvitie <lauri.talvitie@hotmail.com>
2024-03-31 00:09:47 +0200

    edited props

--- src/components/RouteOnMap.js
+++ src/components/RouteOnMap.js
@@ -3,14 +3,18 @@ import MapContainer from "./MapContainer";
 import { Typography } from "@mui/material";
 
 const RouteOnMap = ({ loader, ride }) => {
-  // State to hold the route data
 
   return (
     <div>
-        <Typography variant="h4" align="center" gutterBottom style={{ color: 'white' }}>
-          <b>CommunityGo</b>
-        </Typography>
-        <MapContainer route={ride.routes[0].overview_polyline} loader={loader} height="710px" />
+      <Typography variant="h4" align="center" gutterBottom style={{ color: 'white' }}>
+        <b>CommunityGo</b>
+      </Typography>
+      <MapContainer
+        ride={ride}
+        polyline={ride.routes[0].overview_polyline}
+        loader={loader}
+        height="710px"
+      />
     </div>
   );
 };
