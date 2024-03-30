363d3356268b27061ce3043810957f97a4813a3b
Lauri Talvitie <lauri.talvitie@hotmail.com>
2024-03-31 00:07:06 +0200

    added markers

--- src/components/MapContainer.js
+++ src/components/MapContainer.js
@@ -1,18 +1,22 @@
 import React, { useEffect, useRef } from "react";
 
-const MapContainer = ({ route, loader, height }) => {
+const MapContainer = ({ polyline, ride, loader, height }) => {
   const mapContainerRef = useRef(null);
-
+  
   useEffect(() => {
-    loader.load().then((google) => {
-        // Create a new map instance with specified options and set it to the map container reference.
-        const mapInstance = new google.maps.Map(mapContainerRef.current, {
-          center: { lat: 60.4518, lng: 22.2666 }, // Coordinates for Turku
-          zoom: 12,
-          disableDefaultUI: true,
-          });
+    const loadMap = async () => {
+      const google = await loader.load();
+      const { PinElement } = await google.maps.importLibrary(
+        "marker",
+      );
 
-      // The visuality of the map is defined here
+      const mapInstance = new google.maps.Map(mapContainerRef.current, {
+        center: { lat: 60.4518, lng: 22.2666 },
+        zoom: 12,
+        disableDefaultUI: true,
+        mapId: "DEMO_MAP_ID"
+      });
+      
       const grayscaleStyle = new google.maps.StyledMapType(
         [
           { elementType: "geometry", stylers: [{ color: "#333328" }] },
@@ -41,31 +45,66 @@ const MapContainer = ({ route, loader, height }) => {
       mapInstance.mapTypes.set("styled_map", grayscaleStyle);
       mapInstance.setMapTypeId("styled_map");
 
-      renderRoute(google, mapInstance, route);
+      renderRoute(google, mapInstance, polyline);
+
+      const pinGreen = new PinElement({
+        background: 'green',
+        borderColor: 'green',
+        glyphColor: "#005610",
+      });
+      
+      // Add marker for destination if found
+      if (ride && ride.destination && ride.destination.geometry && ride.destination.geometry.location) {
+        new google.maps.marker.AdvancedMarkerElement({
+          position: ride.destination.geometry.location,
+          map: mapInstance,
+          title: ride.destination.name,
+          content: pinGreen.element
+        });
+      }
+
+      // Add marker for pickup if found
+      if (ride && ride.pickup && ride.pickup.geometry && ride.pickup.geometry.location) {
+        new google.maps.marker.AdvancedMarkerElement({
+          position: ride.pickup.geometry.location,
+          map: mapInstance,
+          title: ride.pickup.name,
+        });
+      }
+
+      // Add markers for waypoints if found
+      if (ride && ride.waypoints && Array.isArray(ride.waypoints)) {
+        ride.waypoints.forEach((waypoint, index) => {
+          if (waypoint && waypoint.geometry && waypoint.geometry.location) {
+            new google.maps.marker.AdvancedMarkerElement({
+              position: waypoint.geometry.location,
+              map: mapInstance,
+              title: waypoint.name,
+            });
+          }
+        });
+      }
+    };
+
+    loadMap();
+  }, [polyline, loader, ride]);
+
+  const renderRoute = (google, map, polyline) => {
+    if (!polyline || !polyline.points) {
+      console.error("Polyline data is empty or invalid");
+      return;
+    }
+
+    const decodedPath = google.maps.geometry.encoding.decodePath(polyline.points);
+
+    const routePolyline = new google.maps.Polyline({
+      path: decodedPath,
+      strokeColor: "#5662FF",
+      strokeWeight: 4,
     });
-  }, [route, loader]);
 
-// Function to render route on the map
-const renderRoute = (google, map, route) => {
-  // Check if route exists
-  if (!route || !route.points) {
-    console.error("Route data is empty or invalid");
-    return;
-  }
-
-  // Decode the polyline points
-  const decodedPath = google.maps.geometry.encoding.decodePath(route.points);
-
-  // Create a Polyline object to render the route
-  const routePolyline = new google.maps.Polyline({
-    path: decodedPath,
-    strokeColor: "#FF0000",
-    strokeWeight: 4,
-  });
-
-  // Set the Polyline on the map
-  routePolyline.setMap(map);
-};
+    routePolyline.setMap(map);
+  };
 
   return (
     <div
@@ -75,7 +114,7 @@ const renderRoute = (google, map, route) => {
         width: "99%",
         borderRadius: "5px",
         marginBottom: "10px",
-        border: "2px solid #222222", 
+        border: "2px solid #222222",
       }}
     />
   );
